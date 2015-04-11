0e87aeea6dbf93f2f9133b87f1fcf53ad920e01b
Sorbus <rowan.abraham@gmail.com>
2015-04-11 13:30:08 -0700

    Improved page structures
    I was using ids in many places where classes would have been more
    appropriate; that's fixed now. Hopefully without breaking anything.

--- app/models/registration_token.rb
+++ app/models/registration_token.rb
@@ -2,4 +2,5 @@ class RegistrationToken < ActiveRecord::Base
 	belongs_to :user
 	validates :token, presence: true
 	validates :token, uniqueness: true
+	default_scope { order("created_at DESC") }
 end

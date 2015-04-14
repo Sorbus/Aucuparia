181d520bec41d950a0d3372920ef25f5d2c5eb01
Sorbus <rowan.abraham@gmail.com>
2015-04-14 15:09:28 -0700

    Making registrations work properly

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 	before_filter :check_permissions, :only => [:new, :create, :cancel]
 	skip_before_filter :require_no_authentication
+	before_filter :configure_permitted_parameters, :only => [:create]
  
 	def check_permissions
 		authorize! :create, User
 	end
+	
+	protected
+		def configure_permitted_parameters
+		  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:display_name, :email, :password) }
+		end
 end

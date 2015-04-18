5df0183196d6545cba96b49ed31620f39675cf1f
Sorbus <rowan.abraham@gmail.com>
2015-04-18 16:23:45 -0700

    Improved comment and index display
    Basically, I moved all of the logic for how to display items and
    comments away from the logic for what to display. So now changing that
    will be much easier!
    
    Partials are really useful.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,19 @@ module ApplicationHelper
 	def icon
 		['genderless','mars','mars-stroke','venus','mercury','transgender','transgender-alt','neuter','ban','mars-double','venus-double','venus-mars','tree','bed','bug','bolt','eye','flask','heart']
 	end
+	
+	def display_avatar(user, size)
+		if !user.use_icon
+			'<i class="fa fa-' + icon[user.icon.to_i] + ' fa-' + size + ' fa-fw"></i>'
+		elsif !user.avatar.nil?
+			'<i class="fa fa-' + icon[user.icon.to_i] + ' fa-' + size + ' fa-fw"></i>'
+		else
+			'<i class="user-image user-' + size + '">&nbsp</i>'
+		end
+	#	unless user.avatar.nil? 
+	#		image_tag(user.avatar.file_name) 
+	#	else
+	#		image_tag("/path/to/fallback.jpg")
+	#	end		
+	end
 end

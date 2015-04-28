02953b2adb7b56a224174f673137883dde560e88
Sorbus <rowan.abraham@gmail.com>
2015-04-27 19:04:18 -0700

    Added structure for persistent messages
    Basically, anything sent via the mailbox notifications API will now
    spawn a notification that only goes away for good once it's clicked on.
    
    I'll probably add more fields to the notification model at some point,
    to make this work a bit better - being able to pass the notification
    type and perhaps a boolean to indicate whether it needs to be
    acknowledged would be useful.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,24 @@
 class MessagesController < ApplicationController
 
-#<% noti.mark_as_read(current_user) %>
+	# to send messages:
+	# user.notify(subject, body, obj = nil, sanitize_text = true, notification_code = nil, send_mail = true)
+	# see http://www.rubydoc.info/gems/mailboxer/Mailboxer/Models/Messageable#notify-instance_method
 
 	def destroy
 		@noti = current_user.mailbox.notifications.find_by_id(params[:id])
 		if !@noti.nil?
-			@noti.mark_as_read
+			@noti.mark_as_read(current_user)
+			respond_to do |format|
+				format.js
+				format.html { redirect_to root_path }
+			end
+		else
+			respond_to do |format|
+				format.js
+				format.html { redirect_to root_path }
+			end
 		end
-		redirect_to root_path
+		
 	end
 	
 	def index

3cf3d93475cd104decf6ed4e8e834550a545356e
Sorbus <rowan.abraham@gmail.com>
2015-05-02 20:18:41 -0700

    Tiny bit of caching

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,11 @@
 module WelcomeHelper
+	def cache_key_for_recent_items
+		max_updated_at = Item.where(:deleted => false, :published => true).maximum(:updated_at).try(:utc).try(:to_s, :number)
+		"items/recent-#{max_updated_at}"
+	end
+	
+	def cache_key_for_categories
+		max_updated_at = Category.maximum(:updated_at).try(:utc).try(:to_s, :number)
+		"categories/all-#{max_updated_at}"
+	end
 end
